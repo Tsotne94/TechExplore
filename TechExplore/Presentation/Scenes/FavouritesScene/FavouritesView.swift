7897ac659b04fcbb3e4341f08907bb060da18f6e
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-02-21 01:46:16 +0400

    feat: product cell prototype

--- TechExplore/Presentation/Scenes/FavouritesScene/FavouritesView.swift
+++ TechExplore/Presentation/Scenes/FavouritesScene/FavouritesView.swift
@@ -15,32 +15,50 @@ struct FavouritesView: View {
 
 struct StatementCellView: View {
     var body: some View {
-        ZStack {
-            Image(systemName: "person.fill")
-                .resizable()
-                .scaledToFit()
-                .background(.purple.opacity(0.5))
-                .clipShape(RoundedRectangle(cornerRadius: 20))
-            VStack {
-                Spacer()
-                HStack {
-                    Text("cool")
-                        .foregroundStyle(.white)
+        HStack {
+            ZStack {
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .background(.purple.opacity(0.5))
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                VStack {
                     Spacer()
-                    Button {
-                        print("pressed")
-                    } label: {
-                        Image(systemName: "heart.fill")
-                            .renderingMode(.template)
-                            .tint(.red)
+                    HStack {
+                        Text("cool")
+                            .foregroundStyle(.white)
+                        Spacer()
+                        Button {
+                            print("pressed")
+                        } label: {
+                            Image(systemName: "heart.fill")
+                                .renderingMode(.template)
+                                .tint(.red)
+                        }
+                        
                     }
-
+                    .padding(.horizontal)
                 }
-                .padding(.horizontal)
+                .padding()
             }
-            .padding()
+            .frame(maxWidth: 200, maxHeight: 200)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("some info here")
+                    .font(.system(.title))
+                    .shadow(radius: 2)
+                Text("some more")
+                    .font(.system(.title2, weight: .medium))
+                HStack {
+                    Text("some icon here")
+                    Image(systemName: "bag.fill")
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundStyle(.red)
+                        .frame(maxWidth: 50, maxHeight: 50)
+                }
+            }
         }
-        .frame(maxWidth: 200, maxHeight: 200)
     }
 }
 
