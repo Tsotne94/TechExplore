1cbaa6de136ff24ad18d8ebd8eefbeb0872b778f
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-02-20 22:23:14 +0400

    first commit

---
+++ TechExplore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechExplore
+//
+//  Created by Cotne Chubinidze on 20.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
