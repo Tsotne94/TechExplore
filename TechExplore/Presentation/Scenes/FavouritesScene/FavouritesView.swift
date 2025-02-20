2ccf602922f85a44b2d495d53fffddad2a698409
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-02-21 01:39:15 +0400

    feature: statement cell prototype coded

--- TechExplore/Presentation/Scenes/FavouritesScene/FavouritesView.swift
+++ TechExplore/Presentation/Scenes/FavouritesScene/FavouritesView.swift
@@ -13,6 +13,37 @@ struct FavouritesView: View {
     }
 }
 
+struct StatementCellView: View {
+    var body: some View {
+        ZStack {
+            Image(systemName: "person.fill")
+                .resizable()
+                .scaledToFit()
+                .background(.purple.opacity(0.5))
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+            VStack {
+                Spacer()
+                HStack {
+                    Text("cool")
+                        .foregroundStyle(.white)
+                    Spacer()
+                    Button {
+                        print("pressed")
+                    } label: {
+                        Image(systemName: "heart.fill")
+                            .renderingMode(.template)
+                            .tint(.red)
+                    }
+
+                }
+                .padding(.horizontal)
+            }
+            .padding()
+        }
+        .frame(maxWidth: 200, maxHeight: 200)
+    }
+}
+
 #Preview {
-    FavouritesView()
+    StatementCellView()
 }
