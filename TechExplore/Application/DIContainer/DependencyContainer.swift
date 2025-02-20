f48bcbd9e1dc7b1d0d3fdddf92e74fda195a09d2
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-02-20 23:15:43 +0400

    feature: added dependency container

---
+++ TechExplore/Application/DIContainer/DependencyContainer.swift
@@ -0,0 +1,63 @@
+//
+//  DependencyContainer.swift
+//  TechExplore
+//
+//  Created by Cotne Chubinidze on 20.02.25.
+//
+
+
+
+import Foundation
+
+public final class DependencyContainer {
+    private var modules: [String: Module] = [:]
+    
+    public init() { }
+    
+    deinit { modules.removeAll() }
+}
+
+extension DependencyContainer {
+    func resolve<T>(for name: String? = nil) -> T {
+        let name = name ?? String(describing: T.self)
+        
+        guard let component: T = modules[name]?.resolve() as? T else {
+            fatalError("Dependency '\(T.self)' not resolved")
+        }
+        
+        return component
+    }
+}
+
+public extension DependencyContainer {
+    static var root = DependencyContainer()
+    
+    convenience init(@ModuleBuilder _ modules: () -> [Module]) {
+        self.init()
+        modules().forEach { add(module: $0) }
+    }
+    
+    convenience init(@ModuleBuilder _ module: () -> Module ) {
+        self.init()
+        add(module: module())
+    }
+    
+    @discardableResult
+    func register(@ModuleBuilder _ modules: () -> [Module]) -> Self {
+        modules().forEach { add(module: $0) }
+        return self
+    }
+    
+    func add(module: Module) {
+        modules[module.name] = module
+    }
+    
+    func build() {
+        _ = Self.root
+    }
+    
+    @resultBuilder struct ModuleBuilder {
+        public static func buildBlock(_ modules: Module...) -> [Module] { modules }
+        public static func buildBlock(_ module: Module) -> Module { module }
+    }
+}
---
+++ TechExplore/Domain/Entities/AppState.swift
@@ -0,0 +1,12 @@
+//
+//  AppState.swift
+//  TechExplore
+//
+//  Created by Cotne Chubinidze on 20.02.25.
+//
+
+public enum AppState: String {
+    case onboarding
+    case authentication
+    case mainFlow
+}
