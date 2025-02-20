1d6c476bbccb94e6ea701008b4628f60cc27d59e
tsotne chubinidze <cotne.2004chubinidze@gmail.com>
2025-02-21 00:04:01 +0400

    chore: added empthy views

---
+++ TechExplore/Presentation/Scenes/StatementDetailsScene/StatementDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  StatementDetailsView.swift
+//  TechExplore
+//
+//  Created by Cotne Chubinidze on 21.02.25.
+//
+
+import SwiftUI
+
+struct StatementDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatementDetailsView()
+}
